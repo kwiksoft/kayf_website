d8d3e8ce7b3570a8df2f941dd02edf3aa1c38aee
tharunonline <tharunonline@gmail.com>
2026-05-26 20:42:05 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/kayf/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
